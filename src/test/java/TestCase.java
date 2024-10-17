93f600da4ed82c0aa29d9ee54d31ed51204daf7e
ly <3219747044@qq.com>
2024-10-17 16:43:29 +0800

    pr

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -5,8 +5,8 @@ public class TestCase {
     @Test
     public void testRight() {
         Cal cal = new Cal();
-        assertEquals(cal.Add(1, 2), 4);
-        System.out.println("success");
+        assertEquals(cal.Add(1, 2), 3);
+        System.out.println("pr success");
     }
 
 
